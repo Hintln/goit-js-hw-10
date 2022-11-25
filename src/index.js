760d4f81cec8cf768487cc33c98f36b523e37361
Hintln <andrejlityprog@gmail.com>
2022-11-25 11:31:11 +0200

    .

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('search-box');
-const list = document.querySelector('country-list');
-const info = document.querySelector('country-info');
+const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
 
 input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
