e01b1793553e17a65017c5989fa954a4876e25c0
Hintln <andrejlityprog@gmail.com>
2022-11-25 11:34:38 +0200

    .

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-const input = document.querySelector('search-box');
+const input = document.getElementById('search-box');
 const list = document.querySelector('.country-list');
 const info = document.querySelector('.country-info');
 
