a798a2a8760b809bca5a5fd012d0957416592ef1
Hintln <andrejlityprog@gmail.com>
2022-12-09 22:17:30 +0200

    refs+

--- src/index.js
+++ src/index.js
@@ -7,4 +7,13 @@ import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+};
+
+
+
  
